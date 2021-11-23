0d7a5f27868db3afe79d34b3113b9cffbec896c8
Roberto Rosa da Silva <44420577+RobertoRosa7@users.noreply.github.com>
2021-11-22 21:03:48 -0300

    fill

---
+++ js/action04.js
@@ -0,0 +1,19 @@
+$('#result').text('');
+
+function executar() {
+  let txt = '';
+  const fill0 = tf.fill([1], 0);
+  const fill1 = tf.fill([2, 2], 1);
+  const fill2 = tf.fill([2, 2], 2);
+
+  txt += 'Fill: \n';
+  txt += fill0.toString() + '\n\n';
+  txt += fill1.toString() + '\n\n';
+  txt += fill2.toString() + '\n\n';
+
+  exibir(txt);
+}
+
+function exibir(str = '') {
+  $('#result').text(str);
+}
