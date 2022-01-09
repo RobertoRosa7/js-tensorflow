701f1cf09ea799ab3f764eb62e6504b515a4d1f9
Robert <roberto.rosa7@gmail.com>
2022-01-09 09:15:27 -0300

    update

---
+++ rede_neurais_artificiais/index06.js
@@ -0,0 +1,17 @@
+const tf = require('@tensorflow/tfjs');
+
+const model = tf.sequential();
+model.add(tf.layers.dense({ units: 1, inputShape: [1], activation: 'sigmoid' }));
+model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
+
+const x = tf.tensor([1, 0], [2, 1]);
+const y = tf.tensor([[0], [1]]);
+const z = tf.tensor([0]) // z como entrada;
+
+// epochs que pode ser considerado como back propagation
+model.fit(x, y, { epochs: 750 }).then(() => {
+  let output = model.predict(z).dataSync();
+  output = Number(parseFloat(output).toFixed(0));
+  output = tf.tensor(output);
+  output.print();
+});
