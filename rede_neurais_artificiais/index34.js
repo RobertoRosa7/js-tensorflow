198e60688e150f95269adfe6480650dad45a002a
Roberto Rosa <roberto.rosa7@gmail.com>
2022-01-28 08:04:12 -0300

    update rede neurais

--- rede_neurais_artificiais/index34.js
+++ rede_neurais_artificiais/index34.js
@@ -2,16 +2,19 @@ const tf = require('@tensorflow/tfjs'); //?
 
 async function neuralNetwork() {
   const model = tf.sequential();
-  model.add(tf.layers.dense({ units: 1, inputShape: [2], activation: 'tanh' }));
-  model.compile({ loss: tf.losses.meanSquaredError, optimizer: 'sgd' });
+  model.add(tf.layers.dense({ units: 2, inputShape: [2], activation: 'tanh' }));
+  model.add(tf.layers.dense({ units: 1, inputShape: [2], activation: 'sigmoid' }));
+  model.compile({ loss: tf.losses.meanSquaredError, optimizer: tf.train.rmsprop(.05) });
 
   const x = tf.tensor([[0, 0], [0, 1], [1, 0], [1, 1]]);
-  const y = tf.tensor([[0], [1], [1], [1]]); // operador OR
+  const y = tf.tensor([[0], [1], [1], [0]]); // operador OR
   const z = tf.tensor([[0, 0], [0, 1], [1, 0], [1, 1]]) // z como entrada;
 
-  for (let i = 0; i < 750; i++) {
+  for (let i = 1; i <= 1000; i++) {
     let train = await model.fit(x, y);
-    console.log(`taxa de error: ${parseFloat(train.history.loss[0]).toFixed(4)}`);
+    if (i % 10 === 0) {
+      console.log(`taxa de error: ${parseFloat(train.history.loss[0]).toFixed(4)}`);
+    }
   }
   let output = model.predict(z).round();
   output.print(); //?
