dcfed78438d271060faf88a3b94930eabed6c047
Roberto Rosa <roberto.rosa7@gmail.com>
2022-01-28 08:56:45 -0300

    update rede neurais

--- rede_neurais_artificiais/index08.js
+++ rede_neurais_artificiais/index08.js
@@ -1,12 +1,13 @@
 const tf = require('@tensorflow/tfjs');
 
 const model = tf.sequential();
-model.add(tf.layers.dense({ units: 1, inputShape: [1], activation: 'softmax' }));
+model.add(tf.layers.dense({ units: 2, inputShape: [1], activation: 'softmax' }));
+model.add(tf.layers.dense({ units: 1, inputShape: [2], activation: 'softplus' }));
 model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
 
 const x = tf.tensor([1, 0], [2, 1]);
 const y = tf.tensor([[0], [1]]);
-const z = tf.tensor([0]) // z como entrada;
+const z = tf.tensor([1]); // z como entrada;
 
 // epochs que pode ser considerado como back propagation
 model.fit(x, y, { epochs: 750 }).then(() => {
